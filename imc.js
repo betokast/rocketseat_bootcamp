9fc952af10005df08532c09a16595e3e84f1c4cf
Roberto Castro <bkastro@gmail.com>
2020-06-23 16:08:27 -0300

    IMC intial

---
+++ imc.js
@@ -0,0 +1,16 @@
+const nome = "Roberto";
+const peso = "85";
+const altura = 1.72;
+
+const imc = peso / (altura * altura)
+
+if ( imc >= 30) {
+    console.log(`${nome}, você está acima do peso`)
+} else {
+    console.log(`${nome}, você não está acima do peso`)
+}
+
+
+
+
+
